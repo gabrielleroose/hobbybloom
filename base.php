79ccf240655c8f869c5016e4e666f721cca43b02
team18 <team18@silo.luddy.indiana.edu>
2025-12-12 21:01:27 -0500

    Adding CSS directory, basic set-up files. Testing functionality of include base.php statement.

---
+++ base.php
@@ -0,0 +1,14 @@
+ <link href="./css/style.css" rel="stylesheet">
+
+    <header>
+        <nav>
+            <div class = "navbar">
+
+                <button class="nav-button" id="tbutton">Time</button>
+
+                <a class="nav-link" href="index.html">Home</a>
+
+            </div>
+        </nav>
+    </header>
+<script src="./js/site.js"></script>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head> <link href="./css/style.css" rel="stylesheet"> </head>
+<body> 
+<?php 
+#include 'base.php'
+?>
+basic ass text here <<<
+
+</body>
+</html>
