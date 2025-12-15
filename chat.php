fe0365942b64713c17ee70030bbc5b05f7656ceb
martichc <martichc@iu.edu>
2025-12-14 21:19:01 -0500

    fixed linking with base.php and style.css

--- chat.php
+++ chat.php
@@ -4,14 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
-    <link rel="stylesheet" href="../style.css">
+    <link href="./css/style.css" rel="stylesheet"> 
 </head>
 <body>
 
-    <div class="navbar">
-        <a href="index.php" class="nav-link">🏠</a>
-        <a href="chat.php" class="nav-link">💬</a>
-    </div>
+    <?php include 'base.php'; ?>
 
     <div class="page-container">
 
