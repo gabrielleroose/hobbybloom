1914e1e9bd60e85e0947f9d0d410015ab8249bb5
martichc <martichc@iu.edu>
2025-12-14 19:28:02 -0500

    created project.php and finished working on the About the Project page

---
+++ project.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>About the Project</title>
+  <link href="./css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include 'base.php'; ?>
+
+<div class="outter-box">
+  <div class="inner-box">
+    <p>About HobbyBloom</p>
+    <div class="subtext">
+      <p>
+        We are launching "HobbyBloom," a new application.  It is a one-stop shop where you can begin, monitor, and develop your hobbies while creating a community around them.
+      </p>
+      <p>
+        We found that many of the apps available today are extremely specialized, which can be overwhelming to new users.  Our primary objective is to assist individuals in expanding their skill sets by providing an approachable, user-friendly platform.
+      </p>
+      <p>
+        HobbyBloom caters to college-aged people who want to enhance their lives by engaging in constructive hobbies.  Our platform provides community engagement, beginner-friendly guides, tailored recommendations, and adaptive goal-setting.
+      </p>
+      <p>
+        Chris Martin, Jack Bruce, Gabby Roose, and Drew Bauman make up our team.  We contribute a variety of front-end, back-end, UX/UI, and database development expertise to guarantee a seamless, interesting, and encouraging user experience.      </p>
+    </div>
+  </div>
+</div>
+
+<div class="light-box">
+  <h2>Project Features & Benefits</h2>
+  <ul>
+    <li>Hobby modules for learning and feedback</li>
+    <li>Randomized and customizable feeds for new hobbies</li>
+    <li>Social features: posts, likes, comments, and messaging</li>
+    <li>Dashboard with streak tracking, calendars, and progress visualization</li>
+    <li>Circles to join communities of like-minded hobbyists</li>
+    <li>Personalized recommendations and adaptive learning paths</li>
+  </ul>
+</div>
+
+</body>
+</html>
