55a168e446fa77a0dbbba1e37c4d584dffb6a113
jhbruce <jhbruce@iu.edu>
2026-01-31 19:21:53 +0000

    Added two CSS classes, added a JS file, and linked newly-created JS file to achievements.html in order to allow smooth button transition

---
+++ app.js
@@ -0,0 +1,5 @@
+
+// note: "button" is in parameters because of toggleSubMenu("this"), as seen in the achievements page.
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle('show')
+}
