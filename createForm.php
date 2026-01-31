4c76e402aa6aadeade5a5a667bb181263007e28a
drewbaum <drewbaum@iu.edu>
2026-01-31 16:53:57 -0500

    added more feilds to the html part of the module creation, needs to be linked to sql - sql table needs to be expanded upon. Certain aspects of the form need completed

--- createForm.php
+++ createForm.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+// Chnage below to real data base info
 $host = "localhost";
 $dbname = "your_database_name";
 $username = "db_user";
@@ -8,13 +9,13 @@ $password = "db_password";
 
 
 if (!isset($_SESSION['user_id'])) {
-    $_SESSION['user_id'] = 1; // false login for test
+    $_SESSION['user_id'] = 1; // made up login for test
 }
 
 $success = false;
 $error = "";
 
-// form submission 
+// form submission handling below
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $name = trim($_POST["name"]);
@@ -74,8 +75,32 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <label>Description:</label><br>
     <textarea name="description" rows="4" cols="40"></textarea><br><br>
 
+    <label>Number of lessons:</label><br><br>
+    <!-- figure out how to add a working number system that the system can count to make spots for each lesson -->
+
+    <label>Notes:</label><br>
+    <textarea name="description" rows="4" cols="40"></textarea><br><br>
+
+    <label>Recomended experince level</label><br><br>
+    <!-- add drop down with levels -->
+
+    <label>Estimated time of completion:</label>
+    <!-- maybe do a set time format if possible -->
+    <input type="text" name="name" required><br><br>
+
+
     <button type="submit">Create Module</button>
 </form>
 
 </body>
 </html>
+
+
+need:  ( module name )
+       ( Module Description )
+        ( Creator )
+        ( number of lessons - way to upload videos -
+            the actual lessons )
+        ( notes section )
+        ( difficult level )
+        ( estimated time )
