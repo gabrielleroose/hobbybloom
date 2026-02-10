8232795893f7a21a42f66b3e0e382eb9dae28698
jhbruce <jhbruce@iu.edu>
2026-02-10 02:26:16 +0000

    Added in error checking at top of share.php

--- pages/share.php
+++ pages/share.php
@@ -1,4 +1,11 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1); //debugging/error messages
+error_reporting(E_ALL);
+
+
+
 session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/db.php';
