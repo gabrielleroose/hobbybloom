3ad824d0bfc1a269c9ff58eb92a23f2f4b0dff23
martichc <martichc@iu.edu>
2026-02-09 19:53:36 -0500

    created db.php for a dedicated file for database

---
+++ pages/db.php
@@ -0,0 +1,24 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$host = "db.luddy.indiana.edu";
+$user = "i494f25_team18";
+$password = "berms2227penes";
+$database = "i494f25_team18";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$database;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $conn = new PDO($dsn, $user, $password, $options);
+} catch (\PDOException $e) {
+
+}
+?>
