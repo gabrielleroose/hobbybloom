1506396e36d09fc981c8ce67e0cd6ec4ac2c416f
groose <groose@iu.edu>
2026-02-10 14:24:49 -0500

    Re-trying

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,7 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once $_SERVER['DOCUMENT_ROOT'] . '/pages/base.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/pages/base.php';
+require_once __DIR__ . '/base.php';
 
 $streak = 1;
 
