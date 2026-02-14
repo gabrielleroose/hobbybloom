c2b540b00d4d33caa9734359f6546a6e22e04c88
drewbaum <drewbaum@iu.edu>
2026-02-14 16:13:04 -0500

    fixed some display and logic errors in createFrom

--- pages/createForm.php
+++ pages/createForm.php
@@ -1,6 +1,15 @@
-
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 1);
+
 require_once 'db.php';
+
+
+
+require_once 'db.php';
+
+$success = false;
+$error = "";
 
 
 // form submission handling below 
