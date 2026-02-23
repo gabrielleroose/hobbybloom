5fd961b576cf356132d5112a820fa30946851a1e
groose <groose@iu.edu>
2026-02-23 00:14:01 -0500

    Structuring share.php html and adding nav and footer

--- pages/share.php
+++ pages/share.php
@@ -10,6 +10,8 @@ session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/twig.php';
+require_once __DIR__ . '/base.php';
+
 
 //get user's Google ID from session
 
@@ -75,4 +77,23 @@ foreach ($mid_ids as $mid_id) {
     echo $twig->render('share.twig', [
     'modules' => $modules
 ]);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Share</title>
+
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/nav.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include __DIR__ . '/../includes/footer.php'; ?>
+</body>
+</html>
 
