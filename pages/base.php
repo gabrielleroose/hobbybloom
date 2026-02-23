be4c3166761dcfa7bc182ffa016aefcbaf3fe94e
jhbruce <jhbruce@iu.edu>
2026-02-23 21:18:12 +0000

    Changed link from modules.php to modules_display.php in base.php

--- pages/base.php
+++ pages/base.php
@@ -51,7 +51,7 @@ require_once 'db.php';
 
                 <a class="menu-item" href="dashboard.php">Dashboard</a>
                 <a class="menu-item" href="activity.php">Activity</a>
-                <a class="menu-item" href="module.php">Modules</a>
+                <a class="menu-item" href="modules_display.php">Modules</a>
                 <a class="menu-item" href="circles.php">Circles</a>
                 <a class="menu-item" href="chat.php">Chat</a>
                 <a class="menu-item wide-view" href="account.php">Account</a>
