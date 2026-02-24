28e298fd9e85677fcb156a0b7cc7d7e1d2439e57
jhbruce <jhbruce@iu.edu>
2026-02-24 08:09:45 +0000

    Added insertion statements to check_answer.php to track each answer the userhas entered.

--- pages/check_answer.php
+++ pages/check_answer.php
@@ -2,6 +2,16 @@
 require 'db.php';
 header('Content-Type: application/json');
 
+session_start();
+$googleId = $_SESSION['google_id'] ?? null;
+
+$user_id_sql = "SELECT id FROM users WHERE google_id = :gid";
+$stmt = $conn->prepare($user_id_sql);
+$stmt->execute([':gid' => $googleId]);
+
+$user_id = $stmt->fetchColumn();
+ 
+
 
 $data = json_decode(file_get_contents('php://input'), true); //reads incoming json data
 $answer_id = $data['answer_id'] ?? null; //assigns null value if answer_id not found, same with stage_num
@@ -12,13 +22,19 @@ if (!$answer_id) {
     exit;
 }
 
-// Fetch the answer's correctness from the database
+// checks if answer is correct
 $stmt = $conn->prepare("SELECT is_correct FROM module_stage_questions_answers WHERE id = ?");
 $stmt->execute([$answer_id]);
 $is_correct = $stmt->fetchColumn();
 
-if ($is_correct === false) {
-    echo json_encode(['correct' => false]);
-} else {
-    echo json_encode(['correct' => $is_correct == 1]);
+if ($is_correct !== false) { //checks if $is_correct is empty
+    $insert = $conn->prepare("
+        INSERT INTO module_stage_questions_user_answers (uid, msqaid) 
+        VALUES (?, ?)
+    ");
+    $insert->execute([$user_id, $answer_id]);
 }
+
+// return correctness
+echo json_encode(['correct' => $is_correct == 1]);
+?>
