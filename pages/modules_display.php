2858cc1d717745854fcd9e6019e3d9dff12b830c
jhbruce <jhbruce@iu.edu>
2026-02-27 04:59:26 +0000

    Began backend for editing; wrote SQL handling code to fetch results if they exist (initialized via module ID sent through 'edit' button on modules_display.php.

--- pages/modules_display.php
+++ pages/modules_display.php
@@ -100,6 +100,7 @@ $mod_id_list = [];
 
                         <?php if ($mod['cid'] == $user_id): ?> <!-- DELETE MODULE BUTTON. CHECKS IF CID = USER_ID. --->
                             <button type="submit" class="module_display_delete_button" name="module_delete" value="<?= $mod['id']?>">Delete Module</button>
+                            <button type="submit" class="module_display_edit_button" name="module_edit" value="<?= $mod['id']?>">Delete Module</button>
                         <?php endif ?>
 
                     </form>
