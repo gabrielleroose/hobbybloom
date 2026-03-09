cccf9a2e012d2a1331d60cbe61c868afd7eb5955
drewbaum <drewbaum@iu.edu>
2026-03-09 10:12:49 -0400

    updating google cal to work deplyed and no tjust local

--- pages/Google_calendar_connect.php
+++ pages/Google_calendar_connect.php
@@ -10,7 +10,11 @@ if (!isset($_SESSION['user']['id'])) {
 
 define('GOOGLE_CLIENT_ID',     '1011869688630-kl05vvf13cg6u6d1tlo9rnj0l4kj7rvn.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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'); 
-define('GOOGLE_REDIRECT_URI',  'http://localhost:8000/pages/google_calendar_callback.php');
+$isLocal = ($_SERVER['HTTP_HOST'] === 'localhost:8000');
+define('GOOGLE_REDIRECT_URI', $isLocal 
+    ? 'http://localhost:8000/pages/google_calendar_callback.php'
+    : 'https://cgi.luddy.indiana.edu/~team18/pages/google_calendar_callback.php'
+);
 
 
 $state = bin2hex(random_bytes(16));
