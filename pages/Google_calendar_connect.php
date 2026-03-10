ee2796557b9ebc072ec83830c35a576c4dc11af7
drewbaum <drewbaum@iu.edu>
2026-03-10 00:09:32 -0400

    adding localhost 3000 to google calendar

--- pages/Google_calendar_connect.php
+++ pages/Google_calendar_connect.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user']['id'])) {
 
 define('GOOGLE_CLIENT_ID',     '1011869688630-kl05vvf13cg6u6d1tlo9rnj0l4kj7rvn.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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'); 
-$isLocal = ($_SERVER['HTTP_HOST'] === 'localhost:8000');
+$isLocal = in_array($_SERVER['HTTP_HOST'], ['localhost:8000', 'localhost:3000']);
 define('GOOGLE_REDIRECT_URI', $isLocal 
     ? 'http://localhost:8000/pages/Google_calendar_callback.php'
     : 'https://cgi.luddy.indiana.edu/~team18/pages/Google_calendar_callback.php'
