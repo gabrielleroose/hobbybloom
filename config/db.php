f959e1eea3f1f61ea2292a0dfaba5cba12ba942c
groose <groose@iu.edu>
2026-06-24 18:26:48 -0400

    Updating db config to use Railway environment variables from IU Luddy Server

--- config/db.php
+++ config/db.php
@@ -1,34 +1,26 @@
 <?php
 
+$host     = getenv('MYSQLHOST');
+$user     = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port     = getenv('MYSQLPORT') ?: '3306';
+$charset  = "utf8mb4";
 
-$host = "db.luddy.indiana.edu";
-$user = "i494f25_team18";
-$password = "berms2227penes";
-$database = "i494f25_team18";
-$charset = "utf8mb4";
-
-$dsn = "mysql:host=$host;dbname=$database;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-
-// use of PDO's for security, mysqli extensions largely outdated.
-
+$dsn = "mysql:host=$host;port=$port;dbname=$database;charset=$charset";
 
 try {
-    $pdo = new PDO(dsn: $dsn, username: $user, password: $password, options: [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-    } catch (PDOException $e) {
-        die("database connection failed: " . $e->getMessage());
-    }
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ]);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
 
 $loader = new \Twig\Loader\FilesystemLoader(paths: __DIR__ . '/../templates');
 
 $twig = new \Twig\Environment(loader: $loader, options: [
     'debug' => true,
 ]);
-
